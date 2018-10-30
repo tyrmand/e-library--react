3d8c17f8518c36577392cb08b1803e2b5f634807
Michał Surmacz <michals.git@gmail.com>
2018-10-30 18:05:07 +0100

    Moves the Books component from the App

--- js/App.jsx
+++ js/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Header from './Header.jsx';
 import Select from './Select.jsx';
-import Books from './Books.jsx';
 import styles from '../scss/style.scss';
 
 const App = () => (
     <div>
         <Header/>
-        <Select/>
-        <Books/>            
+        <Select/>            
     </div>
 )
 
